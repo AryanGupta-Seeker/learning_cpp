71936739cc1bcb45fc82fdaab3f9e37a322486d3
AryanGupta-Seeker <aryangupta.seeker@gmail.com>
2025-06-22 00:08:55 +0530

    prgm24

---
+++ 24th.cpp
@@ -0,0 +1,15 @@
+// Compare two string
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a="apple",b="banana";
+    if(a==b)
+    {
+        cout<<"Both strings are equal"<<endl;
+    }
+    else
+    {
+        cout<<"Strings are not equal"<<endl;
+    }
+}
