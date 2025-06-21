eb4486db2083c4713c224531ba9d45ec672279b5
AryanGupta-Seeker <aryangupta.seeker@gmail.com>
2025-06-21 22:34:00 +0530

    prgm12

---
+++ 12th.cpp
@@ -0,0 +1,14 @@
+// Find the sum of the array using arry
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5], sum = 0;
+    cout << "Enter 5 numbers: ";
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cout << "Sum of the array is: " << sum << endl;
+    return 0;
+}
