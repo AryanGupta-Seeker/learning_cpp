6f8dd1a195a3d21678ae116be7c5d9489aa6df17
AryanGupta-Seeker <aryangupta.seeker@gmail.com>
2025-06-24 14:08:16 +0530

    program9

---
+++ day-4/prgm9.cpp
@@ -0,0 +1,16 @@
+// Swap two number
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int x = 10, y = 20;
+    swap(x, y);
+    cout << "After Swap: x = " << x << ", y = " << y << endl;
+    return 0;
+}
