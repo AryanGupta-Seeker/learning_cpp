9376f841062de78d8c1be48c815a4097377651f6
AryanGupta-Seeker <aryangupta.seeker@gmail.com>
2025-06-24 10:59:03 +0530

    program9

---
+++ Day-3/prgm9.cpp
@@ -0,0 +1,14 @@
+// swap two number using third varaible
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,temp;
+    cout<<"Enter two numbers :";
+    cin>>a>>b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"After swap :a="<<a<<" , b ="<<b;
+    return 0;
+}
