8141b863014a0751201da9f8c3cf163657f378e9
AryanGupta-Seeker <aryangupta.seeker@gmail.com>
2025-06-24 12:17:16 +0530

    program4

--- day-4/prgm4.cpp
+++ day-4/prgm4.cpp
@@ -1,1 +1,14 @@
-// 
+// Square root
+#include<iostream>
+#include<cmath>
+using namespace std;
+double squareRoot(double n){
+    return sqrt(n);
+}
+int main(){
+    double n;
+    cout<<"Enter a number :";
+    cin>>n;
+    cout<<"square root is :"<<squareRoot(n);
+    return 0;
+}
